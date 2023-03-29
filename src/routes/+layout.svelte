516539876cb0a9582fff321b4816dc42522af0c6
Ben Sooter <sooterbw@gmail.com>
2023-03-28 19:36:33 -0500

    Clone SvelteKite and add Tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import "../app.css";
+</script>
+  
+<div class="w-screen h-screen flex flex-col items-center justify-center space-y-10">
+    <slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1 class="text-2xl">Welcome to the <span class="text-orange-400 font-semibold text-3xl">SvelteKit</span> & <span class="text-sky-400 font-semibold text-3xl">Tailwind</span> starter template</h1>
+<p>Visit <a class="text-sky-500 hover:underline" href="https://github.com/sooterbw/sveltekit-tailwind-template">this github rep</a> to read the documentation</p>
